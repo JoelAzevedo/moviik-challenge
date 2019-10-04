d7e68e78f28face745f5172ae4674cce346ce839
Joel <joel.azevedo@gmail.com>
2019-10-04 15:45:57 +0100

    Create a simple route to quotes list and redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { QuotesListComponent } from './components/quotes-list/quotes-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'quotes-list', pathMatch: 'full' },
+  { path: 'quotes-list', component: QuotesListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
