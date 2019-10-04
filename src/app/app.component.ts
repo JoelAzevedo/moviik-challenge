ba3f574d91d82c4a5b9afa839220f66d7d208fe6
Joel <joel.azevedo@gmail.com>
2019-10-04 15:45:18 +0100

    Remove logic and structure from app.component. Only has router outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { QuotesService } from './shared/quotes.service';
-import { Quote } from './shared/quote';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent  {
 
-  quotes: Quote[];
-  dummyQuotes: Array<object>;
-
-  constructor(private quotesService: QuotesService) { }
-
-  ngOnInit() {
-    // this.loadQuotesByPage(1); // test for page 1
-
-    // Dummy quotes list
-    this.quotesService.getDummyQuotes().subscribe(data => {
-      this.dummyQuotes = data;
-    });
-  }
-
-  // Quotes list by page
-  loadQuotesByPage(page: number) {
-    return this.quotesService.getQuotesByPage(page).subscribe((data) => {
-      // console.log(data);
-      this.quotes = data;
-    });
-  }
+  constructor() { }
 
 }
