a0d3253dcc2de0936087a944fdadce895ae7305f
Joel <joel.azevedo@gmail.com>
2019-10-04 15:44:17 +0100

    Create quotes-list component and move logic and service call to it

---
+++ src/app/components/quotes-list/quotes-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Quote } from 'src/app/shared/quote';
+import { QuotesService } from 'src/app/shared/quotes.service';
+
+@Component({
+  selector: 'app-quotes-list',
+  templateUrl: './quotes-list.component.html',
+  styleUrls: ['./quotes-list.component.scss']
+})
+export class QuotesListComponent implements OnInit {
+
+  quotes: Quote[];
+  dummyQuotes: Array<object>;
+
+  constructor(private quotesService: QuotesService) { }
+
+  ngOnInit() {
+    this.loadQuotesByPage(1); // test for page 1
+
+    // Dummy quotes list
+    // this.quotesService.getDummyQuotes().subscribe(data => {
+    //   this.dummyQuotes = data;
+    // });
+  }
+
+  // Quotes list by page
+  loadQuotesByPage(page: number) {
+    return this.quotesService.getQuotesByPage(page).subscribe((data) => {
+      // console.log(data);
+      this.quotes = data;
+    });
+  }
+
+}
