ca590cfca93cdf6feee2e4b4ebb10802e6e38b84
Joel <joel.azevedo@gmail.com>
2019-10-04 03:25:24 +0100

    Define quotes variable and call getQuotesByPage.
    Testing with page 1.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { QuotesService } from './shared/quotes.service';
+import { Quote } from './shared/quote';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'moviik-challenge';
+export class AppComponent implements OnInit {
+
+  quotes: Quote[];
+
+  constructor(private quotesService: QuotesService) { }
+
+  ngOnInit() {
+    this.loadQuotesByPage(1); // test for page 1
+  }
+
+  // Quotes list by page
+  loadQuotesByPage(page: number) {
+    return this.quotesService.getQuotesByPage(page).subscribe((data) => {
+      // console.log(data);
+      this.quotes = data;
+    });
+  }
 }
